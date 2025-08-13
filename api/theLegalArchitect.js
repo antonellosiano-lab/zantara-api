aeda4b4c6269b0a91fcafbf768f4243337a530e3
Antonello <antonellosiano@gmail.com>
2025-08-13 18:33:52 +0800

    add notion logging helper and tests

--- api/theLegalArchitect.js
+++ api/theLegalArchitect.js
@@ -1,3 +1,6 @@
 import { createAgentHandler } from "../handlers/createAgentHandler.js";
+import { writeAgentResult } from "../helpers/notionLogger.js";
 
-export default createAgentHandler("The Legal Architect");
+export default createAgentHandler("The Legal Architect", async (prompt, data) => {
+  await writeAgentResult("theLegalArchitect", prompt, data);
+});
